aa60ce6f8c250472745e6ef4f090d2fd500b1ba2
Jason Janisch <thejasonjanisch@gmail.com>
2016-08-03 11:02:35 -0700

    Moves bottomsheet controller inside queue controller. #1

--- public/angular.js
+++ public/angular.js
@@ -16,11 +16,11 @@ app.config(['$routeProvider', function($routeProvider) {
       });
 }]);
 
-app.controller('bottomSheetController', bottomSheetController);
-function bottomSheetController ($scope, $mdBottomSheet) {
-  $scope.openBottomSheet = function() {
-    $mdBottomSheet.show({
-      template: '<md-bottom-sheet><md-list flex><h4>Bio</h4><p>JavaScript enthusiast, eater of tacos, severely lacking flexibility.</p><h4>Favorite workouts</h4><p>Weight training, cardio, yoga, team sports</p><h4>Availability</h4><p>Sun, Tue, Thur, Sat</p><h4>Interests</h4><p>Gaming, music, snowboarding</p></md-bottom-sheet>'
-    });
-  };
-}
+// app.controller('bottomSheetController', bottomSheetController);
+// function bottomSheetController ($scope, $mdBottomSheet) {
+//   $scope.openBottomSheet = function() {
+//     $mdBottomSheet.show({
+//       template: '<md-bottom-sheet><md-list flex><h4>Bio</h4><p>JavaScript enthusiast, eater of tacos, severely lacking flexibility.</p><h4>Favorite workouts</h4><p>Weight training, cardio, yoga, team sports</p><h4>Availability</h4><p>Sun, Tue, Thur, Sat</p><h4>Interests</h4><p>Gaming, music, snowboarding</p></md-bottom-sheet>'
+//     });
+//   };
+// }
--- public/queue/queue.controller.js
+++ public/queue/queue.controller.js
@@ -2,9 +2,9 @@ var app = angular.module('spotter');
 
 app.controller('queueController', queue);
 
-queue.$inject = ['$http'];
+queue.$inject = ['$http', '$mdBottomSheet', '$scope'];
 
-function queue($http) {
+function queue($http, $mdBottomSheet, $scope) {
   var vm = this;
   vm.match = {};
   vm.getTestUser = function() {
@@ -14,4 +14,12 @@ function queue($http) {
       });
   }
   vm.getTestUser();
+  vm.test = 'Test';
+  vm.openBottomSheet = function() {
+    $mdBottomSheet.show({
+      scope: $scope,
+      template: '<md-bottom-sheet><md-list flex><h4>Test</h4><p>{{queue.test}}</p><h4>Favorite workouts</h4><p>Weight training, cardio, yoga, team sports</p><h4>Availability</h4><p>Sun, Tue, Thur, Sat</p><h4>Interests</h4><p>Gaming, music, snowboarding</p></md-bottom-sheet>'
+    });
+  }
+
 }
