ed5bf3cfec46952b508d3774cdbf4d8021d28619
Jason Janisch <thejasonjanisch@gmail.com>
2016-08-09 00:36:47 -0700

    Adds editor controller and view. #13

--- public/editor/editor.controller.js
+++ public/editor/editor.controller.js
@@ -2,4 +2,28 @@ var app = angular.module('spotter');
 
 app.controller('editorController', editor);
 
-editor.$inject = ['$http'];
+editor.$inject = ['$http', '$scope'];
+
+function editor($http, $scope) {
+  var vm = this;
+  vm.updateInfo = function(myAge, myBio, myWorkouts, myAvailability, myInterests) {
+    var data = {
+      age: myAge,
+      bio: myBio,
+      workouts: myWorkouts,
+      availability: myAvailability,
+      interests: myInterests
+    }
+    var config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    $http.put('/users/You', data, config)
+      .success(function(error, status) {
+        console.log(status);
+      }).error(function(error, status) {
+        console.log(error);
+      });
+  }
+}
